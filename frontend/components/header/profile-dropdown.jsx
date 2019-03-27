12c4fdf919f5823dc0813dbcc32c8e21ced7bb0f
Grant Kopplin <grant.kopplin@gmail.com>
2019-03-27 19:11:38 -0400

    Finish styling for dropdown

--- frontend/components/header/profile-dropdown.jsx
+++ frontend/components/header/profile-dropdown.jsx
@@ -7,14 +7,17 @@ import FollowingIcon from './following-icon';
 export const ProfileDropdown = (props) => {
     return (
         <div className={`profile-dropdown ${props.showDropdown ? '' : 'hidden'}`}>
-            <span>Account</span>
+            <span>
+                Account
+                <button onClick={props.logout} id="logout-link">Log Out</button>
+            </span>
                 <a id="like-link"><LikeIcon /> Likes</a>
                 <a id="following-link"><FollowingIcon /> Following</a>
             <span>Grumblrs</span>
                 <h3>{props.currentUser.username}</h3>
                 <a id="posts-link">Posts</a>
                 <a id="followers-link">Followers</a>
-                <button onClick={props.logout} className="button-logout">Log Out</button>
+                
         </div>
     );
 };
