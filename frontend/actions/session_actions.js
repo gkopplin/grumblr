21d14f3fceac3727d382a863c83e6c25ac3ab581
Grant Kopplin <grant.kopplin@gmail.com>
2019-03-27 14:42:41 -0400

    Create reducers/actions for handling session errors

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,15 +1,20 @@
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 import * as ApiSessionUtil from '../util/api_session_util';
 
 export const signup = user => dispatch => {
     return ApiSessionUtil.createUser(user)
-        .then(user => dispatch(receiveCurrentUser(user)));
+        .then(user => dispatch(receiveCurrentUser(user)),
+            errors => dispatch(receiveSessionErrors(errors))
+        );
 };
 
 export const login = user => dispatch => {
     return ApiSessionUtil.createSession(user)
-        .then(user => dispatch(receiveCurrentUser(user)));
+        .then(user => dispatch(receiveCurrentUser(user)),
+            errors => dispatch(receiveSessionErrors(errors))
+        );
 };
 
 const receiveCurrentUser = currentUser => {
@@ -21,11 +26,20 @@ const receiveCurrentUser = currentUser => {
 
 export const logout = () => dispatch => {
     return ApiSessionUtil.deleteSession()
-        .then( () => dispatch(logoutCurrentUser()) );
+        .then( () => dispatch(logoutCurrentUser()),
+            errors => dispatch(receiveSessionErrors(errors))
+        );
 };
 
 const logoutCurrentUser = () => {
     return {
         type: LOGOUT_CURRENT_USER
+    };
+};
+
+const receiveSessionErrors = errors => {
+    return {
+        type: RECEIVE_SESSION_ERRORS,
+        errors
     };
 };
