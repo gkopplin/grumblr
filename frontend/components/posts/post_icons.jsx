7ae6c7ca320ef5d6e82e4c7a0b4c4d9dc5347de2
Grant Kopplin <grant.kopplin@gmail.com>
2019-03-28 12:05:25 -0400

    Create post icons for creating a post

---
+++ frontend/components/posts/post_icons.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="post-icons">
+            <div>
+                <span className="text-icon">Aa</span>
+                <span>Text</span>
+            </div>
+            <div>
+                <span className="text-icon">tbd</span>
+                <span>Photo</span>
+            </div>
+            <div>
+                <span className="text-icon">tbd</span>
+                <span>Quote</span>
+            </div>
+            <div>
+                <span className="text-icon">tbd</span>
+                <span>Link</span>
+            </div>
+            <div>
+                <span className="text-icon">tbd</span>
+                <span>Chat</span>
+            </div>
+            <div>
+                <span className="text-icon">tbd</span>
+                <span>Audio</span>
+            </div>
+            <div>
+                <span className="text-icon">tbd</span>
+                <span>Video</span>
+            </div>
+            
+        </div>
+    );
+};
