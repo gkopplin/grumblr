f4600ab824c14a2cce9ae1a772818aba4cd66485
Grant Kopplin <grant.kopplin@gmail.com>
2019-03-29 12:15:52 -0400

    Add seed data for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!({username: 'Demo User', email: 'demo_user@demo_email.com', password: 'demo_password'})
+User.delete_all
+Post.delete_all
+
+user_one = User.create!({username: 'Demo User', email: 'demo_user@demo_email.com', password: 'demo_password'})
+user_two = User.create!({username: 'Seed Data User', email: 'seed@seed_email.com', password: 'seed_password'})
+Post.create!([{post_type: 'text', content: 'post number one', author_id: user_two.id},
+              {post_type: 'text', content: 'post number two', author_id: user_two.id} ])
