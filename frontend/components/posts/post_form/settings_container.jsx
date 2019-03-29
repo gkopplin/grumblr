3745965fb3701ef6ce5b33640412e97de6583393
Grant Kopplin <grant.kopplin@gmail.com>
2019-03-29 11:19:50 -0400

    Create container for edit and delete post buttons
    Default edit values no longer working

--- frontend/components/posts/post_form/settings_container.jsx
+++ frontend/components/posts/post_form/settings_container.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {openModal} from '../../../actions/modal_actions';
+import {connect} from 'react-redux';
 
-export default props => {
+const SettingsContainer = props => {
     return (
-        <Link to={`/posts/${props.post.id}/edit`}>Edit</Link>
-        <button onClick={props.openModal}>Delete</button>
+        <div className={props.showSettings ? "settings-container" : "hidden"}>
+            <button onClick={() => props.openModal("update", props.post)}>Edit</button>
+            <button onClick={props.openModal}>Delete</button>
+        </div>
     );
 };
+
+const mdp = dispatch => {
+    return {
+        openModal: (modal, post) => dispatch(openModal(modal, post))
+    };
+};
+
+export default connect(null, mdp)(SettingsContainer);
--- frontend/components/posts/post_item.jsx
+++ frontend/components/posts/post_item.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import SettingsIcon from './post_form/settings_icon';
-import PostModal from './post_modal';
-import {Link} from 'react-router-dom';
-import PostDeleteModal from './post_delete_modal';
+import SettingsContainer from './post_form/settings_container';
 
 
 class PostItem extends React.Component{ 
     constructor(props) {
         super(props);
         this.state = { 
-            ownPost: this.props.currentUser === this.props.post.author_id
+            ownPost: this.props.currentUser === this.props.post.author_id,
+            showSettings: false
         };
+        this.togglePostSettings = this.togglePostSettings.bind(this);
+    }
+
+    togglePostSettings () {
+        this.setState({showSettings: this.state.showSettings ? false : true});
     }
 
     render() {
@@ -18,8 +22,8 @@ class PostItem extends React.Component{
             <div className="post-item">
                 <span className="post-author">{this.props.author.username}</span>
                 <div className="post-content">{this.props.post.content}</div>
-                <SettingsIcon ownPost = {this.state.ownPost}/>
-                <button onClick={() => this.props.openModal("update", this.props.post)}>Edit</button>
+                <SettingsIcon ownPost = {this.state.ownPost} togglePostSettings={this.togglePostSettings}/>
+                <SettingsContainer showSettings = {this.state.showSettings}/>
             </div>
         );
     }
