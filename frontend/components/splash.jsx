6f255bd21d9a7426fa83577f97b9771e75463d3f
Grant Kopplin <grant.kopplin@gmail.com>
2019-04-09 09:33:14 -0400

    Comment out carousel for now

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -31,29 +31,32 @@ class Splash extends React.Component{
             <Header loggedIn={false}/>
             <div className={this.state.animate ? "splash-animate" : "splash-container"}>
 
-            <div className="splash-bg" style={{backgroundImage: `url(${window.splashBG})`}}></div>
-            <div className="splash">
-                <div className="session-container">
-                    <h1 className="main-logo">grumblr</h1>
-                    <p className="tag-line">For everything you love to hate</p>
-                    <div className="button-register">
-                        <Link to="/signup">Get Started</Link>
-                    </div><br/>
-                    <div className="button-login">
-                        <Link to="/login" id="link-login">Log In</Link>
-                    </div><br/>
-                    <div className="button-demo" id="button-demo">
-                        <a onClick={this.demoLogin}>Demo Login</a>
+                <div className="splash-bg" style={{backgroundImage: `url(${window.splashBG})`}}></div>
+                <div className="splash">
+                    <div className="session-container">
+                        <h1 className="main-logo">grumblr</h1>
+                        <p className="tag-line">For everything you love to hate</p>
+                        <div className="button-register">
+                            <Link to="/signup">Get Started</Link>
+                        </div><br/>
+                        <div className="button-login">
+                            <Link to="/login" id="link-login">Log In</Link>
+                        </div><br/>
+                        <div className="button-demo" id="button-demo">
+                            <a onClick={this.demoLogin}>Demo Login</a>
+                        </div>
+                        {/* <button onClick={() => this.animate()}>animate</button> */}
                     </div>
-                    <button onClick={() => this.animate()}>animate</button>
                 </div>
-            </div>
 
             </div>
 
-            <div className={this.state.animate ? "splash-animate" : "splash-container"}>
-            <div className="splash-bg" style={{ backgroundColor: "#8A6DFF"}}></div>
-            </div>
+            {/* <div className={this.state.animate ? "splash-animate" : "splash-container"}>
+                <div className="splash-bg" style={{ backgroundColor: "#8A6DFF"}}></div>
+                <div className="splash">
+                    <h1>Grumblr is for grumpy people</h1>
+                </div>
+            </div> */}
             </>
         );
     }
